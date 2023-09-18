ba62ec273519b6921b8637de0af55fc2ec8986e2
Emily Miller <emilyrauch3@gmail.com>
2023-09-18 13:30:45 -0400

    init commit

---
+++ proj-07-1-contact-list/src/main/java/org/example/ContactListApp.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ContactListApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+
+
+}
