c3fb6038aed9c9f37ddb969d04ddd56a3b0a232c
Emily Miller <emilyrauch3@gmail.com>
2023-10-11 10:08:10 -0400

    Challenge project using control statements

---
+++ challenge-1-arrowhead-control-statements/src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter a whole number: ");
+
+        if (scanner.hasNextInt()) {
+            int number = scanner.nextInt();
+            char arrow = '>';
+
+            for (int i = 1; i <= number; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print(arrow);
+                }
+                System.out.println();
+            }
+
+            for (int i = (number -1); i >= 1; i--) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print(arrow);
+                }
+                System.out.println();
+            }
+
+        }
+
+
+    }
+}
